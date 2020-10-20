d38d201d24ca359981168114bf293dfd826d6f1c
Nicholas Morrow <nicholas.morrow@tempus.com>
2020-10-20 16:34:34 -0500

    Optionally include file extension when restoring a database.

--- src/modules/core/getFileNameFromLabel.ts
+++ src/modules/core/getFileNameFromLabel.ts
@@ -1,6 +1,10 @@
 export const getFileNameFromLabel = (label: string): string => {
   const FILE_EXTENSION = "bak";
   const formattedLabel = label.replace(/ /gi, "_");
-  const fileName = `${formattedLabel}.${FILE_EXTENSION}`;
+  const labelContainsFileExtension = label.includes(".");
+  let fileName = formattedLabel;
+  if (!labelContainsFileExtension) {
+    fileName += `.${FILE_EXTENSION}`;
+  }
   return fileName;
 };
